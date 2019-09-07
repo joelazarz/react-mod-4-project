af450489a49dc48c0fe014f44671618849c00bc5
joe lazarz <jlazarz87@gmail.com>
2019-09-07 15:53:50 -0400

    quick fix

--- src/project-show/ProjectIndex.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import Project from './Project';
-
-ReactDOM.render(<Project />, document.getElementById('root'));
-
-
