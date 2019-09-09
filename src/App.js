6e77a15f7eff025055499d980e4624e0375ada6d
adriennem76 <adrienne.c.miranda@gmail.com>
2019-09-09 11:11:26 -0400

    user profile

--- src/App.js
+++ src/App.js
@@ -1,14 +1,39 @@
 import React, { Component, Fragment } from 'react'
-import { BrowserRouter as Router, Route} from 'react-router-dom'
+import { BrowserRouter as Router, Route, Redirect} from 'react-router-dom'
 import Feed from "./containers/Feed"
+import UserProfile from "./components/UserProfile"
+import ProjectCard from "./components/ProjectCard"
 
 class App extends Component {
 
+  state={
+    users: [],
+    user: {},
+    redirect: ""
+  }
+
+  componentDidMount() {
+    fetch("http://localhost:3000/users/")
+    .then(resp => resp.json())
+    .then(data => this.setState({users: data, user: data[1]}))
+  }
+
+  showProject = (e, project) => {
+    this.setState({redirect: <Redirect to={`/projects/${project.id}`}/>})
+  }
+
+  drawProjectCard = (project) => {
+    return (<ProjectCard project={project} user={this.state.users.find(user => user.id === project.user_id)} key={"project"+project.id} showProject={this.showProject} />)
+  } 
+
   render() {
+    const user = this.state.user
     return (
       <Fragment>
         <Router>
-          <Route path="/projects" component={Feed} />
+          {this.state.redirect}
+          <Route path="/projects" render={() => <Feed showProject={this.showProject}/>} /> 
+          <Route path={`/users/${user.id}`} render={() => <UserProfile user={user} drawProjectCard={this.drawProjectCard}/>} />
         </Router>
       </Fragment>
     )
---
+++ src/components/UserProfile.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import ProjectCard from "./ProjectCard"
+
+const UserProfile = props => {
+
+  const user = props.user
+
+  return (
+    <div>
+      <h1>{user.name}</h1>
+      <h4>{user.github_link}</h4>
+      <h2>My Projects:</h2>
+      <ul>
+        {user.owned_projects.map(project => <li>{props.drawProjectCard(project)}</li>)}
+      </ul>
+      <hr/>
+      <h2>Projects I'm Working On:</h2>
+      <ul>
+      {user.projects.map(project => <li>{props.drawProjectCard(project)}</li>)}
+      </ul>
+    </div>
+  )
+}
+
+export default UserProfile
