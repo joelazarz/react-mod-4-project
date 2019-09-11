db7f91597a10af9d40bb34be43f675903083de0d
joe lazarz <jlazarz87@gmail.com>
2019-09-11 14:00:21 -0400

    navbar changes

--- src/layout/NavBar.js
+++ src/layout/NavBar.js
@@ -6,23 +6,12 @@ import "../App.css";
 
 export class NavBar extends Component {
 
-    state = {
-        login: false
-    }
-
-    componentDidMount(){
-        if (this.props.user)
-        {this.setState({ login: true })}
-    }
-
-
     static propTypes = {
         user: PropTypes.object.isRequired
     }
 
     logOut = () => {
         window.localStorage.removeItem('token')
-        this.setState({ login : false })
     }
 
     render() {
@@ -35,13 +24,14 @@ export class NavBar extends Component {
                 <Link to='/projects' className="btn btn-primary"> Project Feed</Link>
                 <Link to={`/users/${id}`} className="btn btn-primary"> Profile </Link>
                 <Link to='/new-project' className="btn btn-primary"> New Project</Link>
-                {this.state.login ?
+                {!!!name ?
                 <div className='login-status'>
-                    <h5 className='login-badge'><span class="badge badge-success"> Welcome {name}</span></h5>
-                    <Link to='/'><h5 onClick={this.logOut} className='login-badge'><span class="badge badge-dark"> Logout</span></h5></Link>
+                    <Link to='/'><h5 className='login-badge'><span class="badge badge-light">Please Login or Sign Up</span></h5></Link>
                 </div>
-                    : (<div className='login-status'>
-                    <Link to='/'><h5 className='login-badge'><span class="badge badge-light"> Please Login or Sign Up</span></h5></Link>
+                :
+                (<div className='login-status'>
+                    <h5 className='login-badge'><span class="badge badge-success"> Welcome {name}</span></h5>
+                    <Link to='/'><h5 onClick={this.logOut} className='login-badge'><span class="badge badge-dark">Logout</span></h5></Link>
                 </div>)
                 }
                 </div>
